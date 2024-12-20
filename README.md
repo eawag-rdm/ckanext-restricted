2dd755252bc7a27d39617105a56ac99a909e1d58
Christian Förster <christian.foerster@eawag.ch>
2024-12-20 10:28:22 +0100

    all clean

---
+++ README.md
@@ -0,0 +1,102 @@
+[![Tests](https://github.com/eawag-rdm/ckanext-restricted/workflows/Tests/badge.svg?branch=eric)](https://github.com/eawag-rdm/ckanext-restrcited/actions)
+
+# ckanext-restricted
+
+This CKAN extension allows for more granular access control and is based on the work of the EnviDat team of WSL.
+The deployment branch is called for **ERIC** is called `eric`. y
+
+## Requirements
+
+Compatibility with core CKAN versions:
+
+| CKAN version    | Compatible?   |
+| --------------- | ------------- |
+| 2.6 and earlier | not tested    |
+| 2.7             | not tested    |
+| 2.8             | not tested    |
+| 2.9             | Yes           |
+| 2.10            | partially tested |
+
+
+
+## Installation
+
+To install ckanext-restricted:
+
+1. Activate your CKAN virtual environment, for example:
+
+     . /usr/lib/ckan/default/bin/activate
+
+2. Clone the source and install it on the virtualenv
+
+    git clone https://github.com/eawag-rdm/ckanext-restricted.git
+    cd ckanext-restricted
+    pip install -e .
+	pip install -r requirements.txt
+
+3. Add `restricted` to the `ckan.plugins` setting in your CKAN
+   config file (by default the config file is located at
+   `/etc/ckan/default/ckan.ini`).
+
+4. Restart CKAN. For example if you've deployed CKAN with Apache on Ubuntu:
+
+     sudo service apache2 reload
+
+
+## Config settings
+
+None
+
+## Developer installation
+
+To install ckanext-restricted for development, activate your CKAN virtualenv and
+do:
+
+    git clone https://github.com/eawag-rdm/ckanext-restricted.git
+    cd ckanext-restricted
+    python setup.py develop
+    pip install -r dev-requirements.txt
+
+
+## Tests
+
+To run the tests, do:
+
+    pytest --ckan-ini=test.ini
+
+
+## Releasing a new version of ckanext-restricted
+
+If ckanext-restricted should be available on PyPI you can follow these steps to publish a new version:
+
+1. Update the version number in the `setup.py` file. See [PEP 440](http://legacy.python.org/dev/peps/pep-0440/#public-version-identifiers) for how to choose version numbers.
+
+2. Make sure you have the latest version of necessary packages:
+
+    pip install --upgrade setuptools wheel twine
+
+3. Create a source and binary distributions of the new version:
+
+       python setup.py sdist bdist_wheel && twine check dist/*
+
+   Fix any errors you get.
+
+4. Upload the source distribution to PyPI:
+
+       twine upload dist/*
+
+5. Commit any outstanding changes:
+
+       git commit -a
+       git push
+
+6. Tag the new release of the project on GitHub with the version number from
+   the `setup.py` file. For example if the version number in `setup.py` is
+   0.0.1 then do:
+
+       git tag 0.0.1
+       git push --tags
+
+## License
+
+[AGPL](https://www.gnu.org/licenses/agpl-3.0.en.html)
--- setup.py
+++ setup.py
@@ -1,98 +1,52 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
-from codecs import open  # To use a consistent encoding
-from os import path
+from pathlib import Path
 
-here = path.abspath(path.dirname(__file__))
+from setuptools import find_packages, setup
 
-# Get the long description from the relevant file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+# Path to the current file
+here = Path(__file__).resolve().parent
+
+# Get the long description from the README.md file
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='''ckanext-restricted''',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.2',
-
-    description='''Data restricting access extension for CKAN''',
+    name="ckanext-restricted",
+    version="0.0.1",
+    description="Eawag access restriction. Depends on ckanext-scheming and ckanext-repeating.",
     long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/espona/ckanext-restricted',
-
-    # Author details
-    author='''Lucia Espona Pernas''',
-    author_email='''lucia.espona@gmail.com''',
-
-    # Choose your license
-    license='AGPL',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    long_description_content_type="text/markdown",
+    url="https://github.com/eawag-rdm/ckanext-restricted",
+    author="Christian Foerster",
+    author_email="Christian.Foerster@eawag.ch",
+    license="AGPL-3.0-or-later",
     classifiers=[
-        # How mature is this project? Common values are
-        # 3 - Alpha
-        # 4 - Beta
-        # 5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
     ],
-
-
-    # What does your project relate to?
-    keywords='''CKAN restrict access resource''',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
-    install_requires=[],
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
+    keywords="CKAN Eawag dataset restriction custom",
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    namespace_packages=["ckanext"],
+    python_requires=">=3.9",
+    install_requires=[
+        # Dependencies should be listed in a `requirements.txt` file.
+    ],
     include_package_data=True,
-    package_data={
-    },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    package_data={},
     data_files=[],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points='''
-        [ckan.plugins]
-        restricted=ckanext.restricted.plugin:RestrictedPlugin
-	[babel.extractors]
-	ckan = ckan.lib.extract:extract_ckan
-    ''',
-
-    # If you are changing from the default layout of your extension, you may
-    # have to change the message extractors, you can read more about babel
-    # message extraction at
-    # http://babel.pocoo.org/docs/messages/#extraction-method-mapping-and-configuration
-    message_extractors={
-        'ckanext': [
-            ('**.py', 'python', None),
-            ('**.js', 'javascript', None),
-            ('**/templates/**.html', 'ckan', None),
+    entry_points={
+        "ckan.plugins": [
+            "restricted=ckanext.restricted.plugin:RestrictedPlugin",
         ],
-    }
+        "babel.extractors": [
+            "ckan = ckan.lib.extract:extract_ckan",
+        ],
+    },
+    message_extractors={
+        "ckanext": [
+            ("**.py", "python", None),
+            ("**.js", "javascript", None),
+            ("**/templates/**.html", "ckan", None),
+        ],
+    },
 )
