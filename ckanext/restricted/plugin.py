fe35381d11524090cf0e79b38169f02b6070ad0b
Christian Förster <christian.foerster@eawag.ch>
2025-12-15 10:14:29 +0100

    Fix AnonymousUser compatibility for CKAN 2.11
    Add hasattr check before calling as_dict() on auth_user_obj since
    AnonymousUser objects in CKAN 2.11 don't have this method.

--- ckanext/restricted/plugin.py
+++ ckanext/restricted/plugin.py
@@ -184,7 +184,7 @@ def restricted_resource_show(context, data_dict=None):
     # custom restricted check
     auth_user_obj = context.get("auth_user_obj", None)
     user_name = ""
-    if auth_user_obj:
+    if auth_user_obj and hasattr(auth_user_obj, 'as_dict'):
         user_name = auth_user_obj.as_dict().get("name", "")
     else:
         if authz.get_user_id_for_username(context.get("user"), allow_none=True):
